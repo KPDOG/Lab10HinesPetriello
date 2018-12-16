747ea7b051b14406b5773a3d01899b35ee791d9e
KPDOG <45828040+KPDOG@users.noreply.github.com>
2018-12-16 15:23:39 -0500

    Update Box.java

--- Day 3/Box.java
+++ Day 3/Box.java
@@ -1,7 +1,21 @@
 import java.awt.*;
 import java.applet.*;
 import java.util.*;
-public class Box
+public class Box extends Point
 {
- 
+    private Point p1;
+    private Point p2;
+    public Box(int x1, int y1, int x2, int y2) {
+        p1 = new Point(x1, y1);
+        p2 = new Point(x2, y2);
+    }
+    public void draw(Graphics g) {
+        g.drawRect(p1.getX(), p1.getY(), p2.getX(), p2.getX());
+        g.drawRect(p1.getX()+50,p1.getY()+50,p2.getX(),p2.getX());
+        g.drawLine(p1.getX(), p1.getY(),p1.getX()+50,p1.getY()+50);
+        g.drawLine(p1.getX(), p1.getY()+p2.getX(), p1.getX()+50,p1.getY()+50+p2.getX());
+        g.drawLine(p1.getX()+p2.getX(),p1.getY(), p1.getX()+50+ p2.getX(),p1.getY()+50);
+        g.drawLine(p1.getX()+p2.getX(),p1.getY()+p2.getX(), p1.getX()+50+p2.getX(),p1.getY()+50+p2.getX());
+      
+    }
 }
